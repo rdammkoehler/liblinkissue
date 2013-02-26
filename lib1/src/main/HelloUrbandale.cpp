80703215afab81fd96b3b9b73f298cf2727dcf87
Rich Dammkoehler <rpd@noradltd.com>
2013-02-26 15:31:31 -0600

    added Lib1 but not linked

---
+++ lib1/src/main/HelloUrbandale.cpp
@@ -0,0 +1,8 @@
+#include "HelloUrbandale.h"
+#include <iostream>
+
+using namespace std;
+
+void HelloUrbandale::SayHello() {
+	cout << "Hello Urbandale!" << endl;
+}
---
+++ lib1/src/main/HelloUrbandale.h
@@ -0,0 +1,11 @@
+#ifndef HELLO_URBANDALE_H
+#define HELLO_URBANDALE_H
+
+class HelloUrbandale {
+public:
+	HelloUrbandale() {}
+	~HelloUrbandale() {}
+	void SayHello();
+};
+
+#endif
