1541926f012955bbca8d8fa08350ffeb653183c2
Rich Dammkoehler <rpd@noradltd.com>
2013-02-26 17:42:35 -0600

    impass

--- doit.sh
+++ doit.sh
@@ -1,4 +1,8 @@
+pushd lib1 && \
+./doit.sh && \
+popd
+
 rm -Rf build/ && \
 cmake -E make_directory build && \
-cmake -E chdir build cmake .. -DCMAKE_VERBOSE_MAKEFILE=TRUE
+cmake -E chdir build cmake .. -DCMAKE_VERBOSE_MAKEFILE=TRUE && \
 cmake --build build
