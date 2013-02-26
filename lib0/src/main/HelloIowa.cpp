28bd0d832c8e5833a59a2445cdcae0d2f38c678a
Rich Dammkoehler <rpd@noradltd.com>
2013-02-26 15:39:20 -0600

    well, it compiles and runs

--- lib0/src/main/HelloIowa.cpp
+++ lib0/src/main/HelloIowa.cpp
@@ -1,8 +1,10 @@
 #include "HelloIowa.h"
 #include <iostream>
+#include "HelloUrbandale.h"
 
 using namespace std;
 
 void HelloIowa::SayHello() {
 	cout << "Hello Iowa!" << endl;
+	HelloUrbandale().SayHello();
 }
