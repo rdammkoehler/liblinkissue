4dc4e83c6f761569b29d4f86bfac306dd07a0267
Rich Dammkoehler <rpd@noradltd.com>
2013-03-01 10:48:31 -0600

    tweak

--- doit.sh
+++ doit.sh
@@ -1,6 +1,6 @@
-pushd lib1 && \
-./doit.sh && \
-popd && \
+#pushd lib1 && \
+#./doit.sh && \
+#popd && \
 rm -Rf build/ && \
 cmake -E make_directory build && \
 cmake -E chdir build cmake .. -DCMAKE_VERBOSE_MAKEFILE=TRUE && \
